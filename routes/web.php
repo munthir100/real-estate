5d5765db1001d1f527e42f208a30fdc8f4a6d72e
munthir100 <munthiromer100@gmail.com>
2024-02-02 15:26:30 +0200

    make a test on nafath

--- routes/web.php
+++ routes/web.php
@@ -15,35 +15,6 @@ use App\Http\Controllers\custom\payments\PaymentController;
 use Twilio\Rest\Client;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test', function () {
-
-    $url = 'https://test-iamservices.semati.sa/nafath/api/v1/client/authorize/';
-    $headers = array(
-        'Content-Type: Application/json',
-        'Authorization: apikey 21c0f19a-fce5-4d4d-b30f-ffd1c3860731'
-    );
-    
-    $data = array(
-        'id' => '2345432345',
-        'action' => 'SpRequest',
-        'service' => 'DigitalServiceEnrollmentWithoutBio'
-    );
-    
-    $options = array(
-        'http' => array(
-            'header'  => implode("\r\n", $headers),
-            'method'  => 'POST',
-            'content' => json_encode($data),
-        ),
-    );
-    
-    $context  = stream_context_create($options);
-    $response = file_get_contents($url, false, $context);
-    
-    // Output the response
-    echo $response;
-    
-
-});
+Route::view('/test','test');
 
 Route::get('payment/callback', [PaymentController::class, 'callback'])->name('payment.callback');
